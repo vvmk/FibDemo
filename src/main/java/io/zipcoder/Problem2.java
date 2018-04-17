b4e75e5d27f67cdd35da480b20eb763d75f434a4
vvmk <vincentmasiello@gmail.com>
2018-04-16 23:34:53 -0400

    Fibcursive

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,16 @@
 package io.zipcoder;
 
 public class Problem2 {
+    static String fiberative(int max) {
+        return "";
+    }
+
+    static String fibcursive(int max) {
+        return "0, 1" + fibcursive(0, 1, max);
+    }
+
+    private static String fibcursive(int a, int b, int max) {
+        int n = a + b;
+        return n >= max ? "" : ", " + n + fibcursive(b, n, max);
+    }
 }
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Problem2Test {
+    @Test
+    void testFibIterative() {
+        int input = 150;
+
+        String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actual = Problem2.fiberative(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFibRecursive() {
+        int input = 150;
+
+        String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actual = Problem2.fibcursive(input);
+
+        assertEquals(expected, actual);
+    }
 }
